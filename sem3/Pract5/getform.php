5e3314c155c76d4b7846154b48702d7f28500710
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-10-23 16:20:48 +0530

    getform.php

---
+++ sem3/Pract5/getform.php
@@ -0,0 +1,12 @@
+<html>
+        <head>
+                <title>Home</title>
+        </head>
+        <body style="padding: 30px; line-height: 2;">
+                Welcome <?php echo $_GET["name"]; ?>!!! <br>
+                Your Roll Number is: <?php echo $_GET["roll"]; ?><br>
+                Age: <?php echo $_GET["age"]; ?><br>
+                Branch: <?php echo $_GET["branch"]; ?><br>
+                Year: <?php echo $_GET["year"]; ?>
+        </body>
+</html>
