38e2331a1b4bb2bb0f22372b6f294c7e81a00b41
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-10-23 16:21:42 +0530

    postform.php

---
+++ sem3/Pract5/postform.php
@@ -0,0 +1,12 @@
+<html>
+        <head>
+                <title>Home</title>
+        </head>
+        <body style="padding: 30px; line-height: 2;">
+                Welcome <?php echo $_POST["name"]; ?>!!! <br>
+                Your Roll Number is: <?php echo $_POST["roll"]; ?><br>
+                Age: <?php echo $_POST["age"]; ?><br>
+                Branch: <?php echo $_POST["branch"]; ?><br>
+                Year: <?php echo $_POST["year"]; ?>
+        </body>
+</html>
