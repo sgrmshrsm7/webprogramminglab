8b4cc9b3c358608707e766913e13aababa12539c
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-11-09 01:23:05 +0530

    db.php

---
+++ sem3/Pract6/Php/db.php
@@ -0,0 +1,19 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = new mysqli($servername, $username, $password);
+if ($conn->connect_error)
+{
+        die("Connection failed: " . $conn->connect_error);
+}
+$sql = "CREATE DATABASE iwp_proj";
+if ($conn->query($sql) === TRUE)
+{
+        echo "Database created successfully";
+}
+else {
+        echo "Error creating database: " . $conn->error;
+}
+$conn->close();
+?>
