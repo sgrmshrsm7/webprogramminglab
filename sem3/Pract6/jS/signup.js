35f6a21f8b6c2dd0fd273018c322301aa1203bec
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-11-13 22:23:14 +0530

    signup.js

--- sem3/Pract6/jS/signup.js
+++ sem3/Pract6/jS/signup.js
@@ -6,6 +6,7 @@ function ValidateData(){
         var b=document.forms["signupform"]["password"].value;
         var c=document.forms["signupform"]["confirmpassword"].value;
         var n=1;
+        var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         if(x.length == 0){
                 alert("First name can't be empty");
                 n=0;
@@ -14,8 +15,8 @@ function ValidateData(){
                 alert("Last name can't be empty");
                 n=0;
         }
-        else if(z.length == 0){
-                alert("Email not entered");
+        else if(!(z.match(mailformat))){
+                alert("Enter valid email");
                 n=0;
         }
         else if(a.length !=10){
@@ -28,11 +29,13 @@ function ValidateData(){
         }
         else if(b != c){
                 alert("Confirm password doesn't match");
+                n=0;
         }
         else{
                 alert("Registered successfully");
         }
         if (n == 0) {
+                alert("Not Registered Successfully");
                 return false;
         }
         else{
